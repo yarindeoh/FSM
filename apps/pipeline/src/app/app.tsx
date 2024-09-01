372befc4fbcbc5a9344f6a69937939ae0a4783b2
yarindeohcld <yarin.dekel@cloudinary.com>
2024-09-01 09:33:53 +0300

    fix(pipeline): local state event transitions

--- apps/pipeline/src/app/app.tsx
+++ apps/pipeline/src/app/app.tsx
@@ -1,72 +1,5 @@
 import styled from 'styled-components';
-import React, { useCallback, useEffect, useState } from 'react';
-
-export type View = {
-  displayName:
-    | 'Code Verification'
-    | 'Code Quality'
-    | 'Build'
-    | 'Publish'
-    | 'Deploy';
-  step: number;
-};
-
-export const mapNameToView: Record<number, View> = [
-  {
-    displayName: 'Code Verification',
-    step: 0,
-  },
-  {
-    displayName: 'Code Quality',
-    step: 1,
-  },
-  {
-    displayName: 'Build',
-    step: 2,
-  },
-  {
-    displayName: 'Publish',
-    step: 3,
-  },
-  {
-    displayName: 'Deploy',
-    step: 4,
-  },
-];
-
-export function App() {
-  const [currentJob, setCurrentJob] = useState(mapNameToView[0]);
-
-  const onTransition = useCallback(() => {
-    setCurrentJob( mapNameToView[currentJob?.step + 1]);
-  }, [currentJob]);
-
-  useEffect(() => {
-    setTimeout(() => {
-      onTransition();
-    }, 2500)
-  }, [onTransition])
-
-
-  return (
-    <StyledApp>
-      <h1>CI Pipeline</h1>
-      <StyledJobsWrapper>
-        {Object.values(mapNameToView).map((step, index) => (
-          <StyledJob
-            isCurrent={step.step === currentJob?.step}
-            key={`${step.displayName}_${index}`}
-          >
-            <div>{step.displayName}</div>
-          </StyledJob>
-        ))}
-      </StyledJobsWrapper>
-      <button onClick={onTransition}>Transition</button>
-    </StyledApp>
-  );
-}
-
-export default App;
+import React, { useCallback, useMemo, useState } from 'react';
 
 const StyledApp = styled.div`
   display: flex;
@@ -88,3 +21,82 @@ export const StyledJob = styled.div<{ isCurrent: boolean }>`
   align-items: center;
   background-color: ${({ isCurrent }) => (isCurrent ? 'red' : 'aliceblue')};
 `;
+
+export type View = {
+  displayName: 'Code Verification' | 'Code Quality' | 'Build' | 'Publish' | 'Deploy';
+  step: number;
+  on: {
+    next: () => string;
+    prev?: () => string;
+  };
+};
+
+export const mapNameToView: Record<string, View> = {
+  verification: {
+    displayName: 'Code Verification',
+    step: 0,
+    on: {
+      next: () => 'quality',
+    },
+  },
+  quality: {
+    displayName: 'Code Quality',
+    step: 1,
+    on: {
+      next: () => 'build',
+      prev: () => 'verification',
+    },
+  },
+  build: {
+    displayName: 'Build',
+    step: 2,
+    on: {
+      next: () => 'publish',
+      prev: () => 'quality',
+    },
+  },
+  publish: {
+    displayName: 'Publish',
+    step: 3,
+    on: {
+      next: () => 'deploy',
+      prev: () => 'build',
+    },
+  },
+  deploy: {
+    displayName: 'Deploy',
+    step: 4,
+    on: {
+      next: () => 'verification',
+      prev: () => 'publish',
+    },
+  },
+};
+
+export function App() {
+  const initialState = useMemo(() => mapNameToView['verification'], []);
+  const [currentJob, setCurrentJob] = useState(initialState);
+
+  const onTransition = useCallback(() => {
+    const sourceState = currentJob.on.next();
+    setCurrentJob(mapNameToView[sourceState]);
+  }, [currentJob]);
+
+  // console.log(currentJob.step, 'currentJob');
+
+  return (
+    <StyledApp>
+      <h1>CI Pipeline</h1>
+      <StyledJobsWrapper>
+        {Object.values(mapNameToView).map((step, index) => (
+          <StyledJob isCurrent={step.step === currentJob.step} key={`${step.displayName}_${index}`}>
+            <div>{step.displayName}</div>
+          </StyledJob>
+        ))}
+      </StyledJobsWrapper>
+      <button onClick={onTransition}>Transition</button>
+    </StyledApp>
+  );
+}
+
+export default App;
