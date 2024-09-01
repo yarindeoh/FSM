b6b4ca11fbeaad4d618f619ad43024171bba4fa3
yarindeohcld <yarin.dekel@cloudinary.com>
2024-09-01 09:17:47 +0300

    fix(pipeline): first version of local naive state transitions

--- apps/pipeline/src/app/app.tsx
+++ apps/pipeline/src/app/app.tsx
@@ -1,16 +1,90 @@
 import styled from 'styled-components';
+import React, { useCallback, useEffect, useState } from 'react';
 
-const StyledApp = styled.div`
-  // Your style here
-`;
+export type View = {
+  displayName:
+    | 'Code Verification'
+    | 'Code Quality'
+    | 'Build'
+    | 'Publish'
+    | 'Deploy';
+  step: number;
+};
+
+export const mapNameToView: Record<number, View> = [
+  {
+    displayName: 'Code Verification',
+    step: 0,
+  },
+  {
+    displayName: 'Code Quality',
+    step: 1,
+  },
+  {
+    displayName: 'Build',
+    step: 2,
+  },
+  {
+    displayName: 'Publish',
+    step: 3,
+  },
+  {
+    displayName: 'Deploy',
+    step: 4,
+  },
+];
 
 export function App() {
+  const [currentJob, setCurrentJob] = useState(mapNameToView[0]);
+
+  const onTransition = useCallback(() => {
+    setCurrentJob( mapNameToView[currentJob?.step + 1]);
+  }, [currentJob]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      onTransition();
+    }, 2500)
+  }, [onTransition])
+
+
   return (
     <StyledApp>
-      <h1>Welcome</h1>
-      <div>nx react v1</div>
+      <h1>CI Pipeline</h1>
+      <StyledJobsWrapper>
+        {Object.values(mapNameToView).map((step, index) => (
+          <StyledJob
+            isCurrent={step.step === currentJob?.step}
+            key={`${step.displayName}_${index}`}
+          >
+            <div>{step.displayName}</div>
+          </StyledJob>
+        ))}
+      </StyledJobsWrapper>
+      <button onClick={onTransition}>Transition</button>
     </StyledApp>
   );
 }
 
 export default App;
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+`;
+
+const StyledJobsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+export const StyledJob = styled.div<{ isCurrent: boolean }>`
+  width: 200px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ isCurrent }) => (isCurrent ? 'red' : 'aliceblue')};
+`;
